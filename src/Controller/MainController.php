50e203dd0449e9b71ac8960422827a6ab844f174
Timothe Raynaud <raynaud.timothe@gmail.com>
2023-02-13 21:50:23 +0100

    :sparkles: Add Home page with email

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -19,7 +19,21 @@ class MainController
 
     public function index()
     {
+        $data = '';
+        if (isset($post['submit'])) {
+            $to = "raynaud.timothe@gmail.com";
+            $subject = $post['subject'];
+            $message = $post['message'];
+            $headers = "From: (Email) " . $post['email'] . " (Firstname) " . $post['firstname'] . " (Lastname) " . $post['lastname'];
+
+            if (mail($to, $subject, $message, $headers)) {
+                $data = 'sendMail';
+            } else {
+                $data = 'errorMail';
+            }
+        }
         echo $this->twig->render('front/pages/home.html.twig', [
+            'data' => $data,
         ]);
     }
 
