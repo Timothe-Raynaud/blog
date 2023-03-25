12be8806196b393efa8063115329b98cd463fff7
Timothe Raynaud <raynaud.timothe@gmail.com>
2023-03-25 13:34:00 +0100

    :wrench: fix for symfony insight

--- public/index.php
+++ public/index.php
@@ -2,7 +2,19 @@
 
 session_start();
 
-require_once dirname(__DIR__) . '/config/config.php';
+define('ROOT', dirname(__DIR__));
+
+require_once(ROOT . '/config/autoloader.php');
+require_once(ROOT . '/vendor/autoload.php');
+
+// Dev/Prod configuration
+const LOCAL_ENVIRONMENT = ROOT . '/config/local.php';
+const PROD_ENVIRONMENT = ROOT . '/config/production.php';
+
+match (file_exists(LOCAL_ENVIRONMENT)){
+    true => require_once(LOCAL_ENVIRONMENT),
+    default => require_once(PROD_ENVIRONMENT)
+};
 
 use Manager\RooterManager;
 
